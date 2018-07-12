51e2646630d987ac7a325e854c3f18dc89fecda0
tanaka <himilk6@yahoo.co.jp>
2018-07-12 18:58:03 +0900

    tweet時刻表示：データ構造を持たせる

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,34 +1,53 @@
 <template lang="pug">
 .container
-  textarea(v-model="tweet")
-  div
+  textarea.post-message(v-model="tweet")
+  div.postbutton
     button(@click="post()")
       | post
     .list-group
       .list-item(v-for="t in timeline")
-        | {{t}}
+        | {{t.tweet}}
+        | {{t.date}}
 </template>
 
 <style lang="sass">
-  textarea
+  *:focus
+    outline: none
+  .container
+    margin: 0 auto
+    width: 40em
+  .post-message
     width: 50em
     height: 10em
   .list-item
     margin: 20px
     padding: 10px
     border: solid
+    text-align: left
     border-color: skyblue
     border-radius: 10px
+  .postbutton
+    text-align: center
   button
-    margin: 0 auto
+    width: 100px
+    font-size: 15px
     padding: 0.5em 1em
+    border: 0 none
+    padding-bottom: 1em
     border-radius: 10px
+    overflow: hidden
     text-decoration: none
     background: skyblue
     color: #fff
+    cursor: pointer
+    &:hover
+      background-color: orange
+
 </style>
 
 <script>
+import moment from 'moment'
+
 export default{
   data(){
     return{
@@ -39,19 +58,29 @@ export default{
   },
   methods: {
     post(){
-      this.tweetList.push(this.tweet)
-      this.timeline = this.reverse()
-      console.log(this.tweetList)
-      console.log(this.timeline)
-      this.clear()
+      if (this.isTweetPresent()){
+        this.tweetList.push({
+          tweet: this.tweet,
+          date: this.tweetTime()
+        })
+        console.log(this.tweetList)
+        this.timeline = this.reverse()
+        this.clear()
+      }
     },
     clear(){
       this.tweet = ''
     },
     reverse(){
-      console.log(this.tweetList)
       var copy = this.tweetList.slice()
       return copy.reverse()
+    },
+    isTweetPresent() {
+      return this.tweet !== ''
+    },
+    tweetTime(){
+      var date = moment()
+      return date.format("HH:mm")
     }
   }
 }
