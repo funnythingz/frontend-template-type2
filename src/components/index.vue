da2ba548e8ed52aadaba6ae9b070ec745defc48a
tanaka <himilk6@yahoo.co.jp>
2018-07-12 15:49:13 +0900

    sliceしてreverseがうまい具合に使えるようになった

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,15 +1,42 @@
 <template lang="pug">
 .container
-  h1.hello
-    | Hello
+  textarea(v-model="tweet")
+  button(@click="post()")
+    | post
+  .list-group
+    .list-item(v-for="t in timeline")
+      | {{t}}
 </template>
 
 <style lang="sass">
-.hello
-  color: #789
+
 </style>
 
 <script>
-export default {
+export default{
+  data(){
+    return{
+      tweet: '',
+      tweetList: [],
+      timeline: []
+    }
+  },
+  methods: {
+    post(){
+      this.tweetList.push(this.tweet)
+      this.timeline = this.reverse()
+      console.log(this.tweetList)
+      console.log(this.timeline)
+      this.clear()
+    },
+    clear(){
+      this.tweet = ''
+    },
+    reverse(){
+      console.log(this.tweetList)
+      var copy = this.tweetList.slice()
+      return copy.reverse()
+    }
+  }
 }
 </script>
