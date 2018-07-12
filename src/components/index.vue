3531b249a4d6e0a6f0a1c94960f1486c809ace9d
tanaka <himilk6@yahoo.co.jp>
2018-07-12 16:30:53 +0900

    postボタンデザイン変更

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,15 +1,31 @@
 <template lang="pug">
 .container
   textarea(v-model="tweet")
-  button(@click="post()")
-    | post
-  .list-group
-    .list-item(v-for="t in timeline")
-      | {{t}}
+  div
+    button(@click="post()")
+      | post
+    .list-group
+      .list-item(v-for="t in timeline")
+        | {{t}}
 </template>
 
 <style lang="sass">
-
+  textarea
+    width: 50em
+    height: 10em
+  .list-item
+    margin: 20px
+    padding: 10px
+    border: solid
+    border-color: skyblue
+    border-radius: 10px
+  button
+    margin: 0 auto
+    padding: 0.5em 1em
+    border-radius: 10px
+    text-decoration: none
+    background: skyblue
+    color: #fff
 </style>
 
 <script>
